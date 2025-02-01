1809ca0f86ec7bf230a475b59af1bfe9a9d4dbdb
Kelvin <kelvindelima710@gmail.com>
2025-02-01 16:26:01 -0300

    feat: http client implemented

--- lib/app/data/http/http_client.dart
+++ lib/app/data/http/http_client.dart
@@ -1,3 +1,14 @@
+import 'package:http/http.dart' as http;
+
 abstract class HttpClientInterface {
   Future get({required String url});
+}
+
+class HttpClient implements HttpClientInterface {
+  final client = http.Client();
+
+  @override
+  Future get({required String url}) async {
+    return await client.get(Uri.parse(url));
+  }
 }
