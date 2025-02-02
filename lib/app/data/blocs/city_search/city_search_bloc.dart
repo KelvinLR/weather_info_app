b43a288a2246191f9e946e139477fe4267757d19
Kelvin <kelvindelima710@gmail.com>
2025-02-02 10:18:38 -0300

    fix: adjusting repositories parameters at CitySearchBloc

--- lib/app/data/blocs/city_search/city_search_bloc.dart
+++ lib/app/data/blocs/city_search/city_search_bloc.dart
@@ -2,20 +2,19 @@ import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lapisco_challenge/app/data/blocs/city_search/city_search_event.dart';
 import 'package:lapisco_challenge/app/data/blocs/city_search/city_search_state.dart';
+import 'package:lapisco_challenge/app/data/http/http_client.dart';
 import 'package:lapisco_challenge/app/data/repositories/city_repository.dart';
 import 'package:lapisco_challenge/app/data/repositories/current_weather_repository.dart';
 import 'package:lapisco_challenge/app/data/repositories/daily_weather_repository.dart';
 
 class CitySearchBloc extends Bloc<CitySearchEvent, CitySearchState> {
-  final CityRepository cityRepository;
-  final CurrentWeatherRepository currentWeatherRepository;
-  final DailyWeatherRepository dailyWeatherRepository;
+  final CityRepository cityRepository = CityRepository(client: HttpClient());
+  final CurrentWeatherRepository currentWeatherRepository =
+      CurrentWeatherRepository(client: HttpClient());
+  final DailyWeatherRepository dailyWeatherRepository =
+      DailyWeatherRepository(client: HttpClient());
 
-  CitySearchBloc({
-    required this.cityRepository,
-    required this.currentWeatherRepository,
-    required this.dailyWeatherRepository,
-  }) : super(CityDataLoading()) {
+  CitySearchBloc() : super(CityDataLoading()) {
     on<SearchCityDataEvent>(_onSearchCityDataEvent);
   }
 
@@ -23,21 +22,17 @@ class CitySearchBloc extends Bloc<CitySearchEvent, CitySearchState> {
       SearchCityDataEvent event, Emitter<CitySearchState> emit) async {
     emit(CityDataLoading());
 
-    try {
-      final cityData = await cityRepository.getCityInformation(
-          event.latitude, event.longitude);
-      final currentWeather = await currentWeatherRepository
-          .getCurrentWeatherData(event.latitude, event.longitude);
-      final dailyWeather = await dailyWeatherRepository.getDailyWeatherData(
-          event.latitude, event.longitude);
+    final cityData = await cityRepository.getCityInformation(
+        event.latitude, event.longitude);
+    final currentWeather = await currentWeatherRepository.getCurrentWeatherData(
+        event.latitude, event.longitude);
+    final dailyWeather = await dailyWeatherRepository.getDailyWeatherData(
+        event.latitude, event.longitude);
 
-      emit(CityDataLoaded(
-        cityData: cityData,
-        currentWeatherData: currentWeather,
-        dailyWeatherData: dailyWeather,
-      ));
-    } catch (e) {
-      emit(CityDataError());
-    }
+    emit(CityDataLoaded(
+      cityData: cityData,
+      currentWeatherData: currentWeather,
+      dailyWeatherData: dailyWeather,
+    ));
   }
 }
