a71cf8d7a201e43fdab10aa91d43225ed0c633e8
Kelvin <kelvindelima710@gmail.com>
2025-02-02 10:19:31 -0300

    feat: add 'name' parameter to CityModel

--- lib/app/data/models/city_model.dart
+++ lib/app/data/models/city_model.dart
@@ -1,28 +1,28 @@
 class CityModel {
+  final String name;
   final double latitude;
   final double longitude;
   final String timeZone;
   final String timeZoneAbbreviation;
   final double elevation;
-  //final Map<String, dynamic> currentInformation;
-  //final List<DailyWeatherModel> dailyInformation;
 
   const CityModel({
+    required this.name,
     required this.latitude,
     required this.longitude,
     required this.timeZone,
     required this.timeZoneAbbreviation,
     required this.elevation,
-    //required this.currentInformation,
-    //required this.dailyInformation,
   });
 
   factory CityModel.fromMap(Map<String, dynamic> map) {
     return CityModel(
-        latitude: map['latitude'],
-        longitude: map['longitude'],
-        timeZone: map['timezone'],
-        timeZoneAbbreviation: map['timezone_abbreviation'],
-        elevation: map['elevation']);
+      name: map['name'],
+      latitude: map['latitude'],
+      longitude: map['longitude'],
+      timeZone: map['timezone'],
+      timeZoneAbbreviation: map['timezone_abbreviation'],
+      elevation: map['elevation'],
+    );
   }
 }
