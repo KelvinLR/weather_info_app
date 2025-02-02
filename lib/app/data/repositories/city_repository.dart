6f554551827162ef007c0bb4c1b891dee9e609ae
Kelvin <kelvindelima710@gmail.com>
2025-02-02 20:01:54 -0300

    feat: add placemartFromCoordinates to convert latitude and longitude into place name at CityRepository

--- lib/app/data/repositories/city_repository.dart
+++ lib/app/data/repositories/city_repository.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:geocoding/geocoding.dart';
 import 'package:lapisco_challenge/app/data/http/exceptions.dart';
 import 'package:lapisco_challenge/app/data/http/http_client.dart';
 import 'package:lapisco_challenge/app/data/models/city_model.dart';
@@ -22,12 +22,17 @@ class CityRepository implements CityRepositoryInterface {
     if (response.statusCode == 200) {
       final body = jsonDecode(response.body);
 
+      List<Placemark> locationInfo =
+          await placemarkFromCoordinates(latitude, longitude);
+
       return CityModel(
-          latitude: body['latitude'],
-          longitude: body['longitude'],
-          timeZone: body['timezone'],
-          timeZoneAbbreviation: body['timezone_abbreviation'],
-          elevation: body['elevation']);
+        name: locationInfo.first.locality ?? "",
+        latitude: body['latitude'],
+        longitude: body['longitude'],
+        timeZone: body['timezone'],
+        timeZoneAbbreviation: body['timezone_abbreviation'],
+        elevation: body['elevation'],
+      );
     } else if (response.statusCode == 404) {
       throw NotFoundException('Invalid URL');
     } else {
