ded0d7ee1e2bd1660f70f12fb6173df54a1856eb
Kelvin <kelvindelima710@gmail.com>
2025-02-02 19:59:22 -0300

    feat: create CurrentLocationModel to get data about current location

--- lib/app/data/models/current_location_model.dart
+++ lib/app/data/models/current_location_model.dart
@@ -1,10 +1,14 @@
 class CurrentLocationModel {
-  final double cityName;
+  final String cityName;
+  final String region;
+  final String country;
   final double latitude;
   final double longitude;
 
   CurrentLocationModel({
     required this.cityName,
+    required this.region,
+    required this.country,
     required this.latitude,
     required this.longitude,
   });
@@ -12,6 +16,8 @@ class CurrentLocationModel {
   factory CurrentLocationModel.fromMap(Map<String, dynamic> map) {
     return CurrentLocationModel(
       cityName: map['city'],
+      region: map['region_code'],
+      country: map['country_name'],
       latitude: map['latitude'],
       longitude: map['longitude'],
     );
