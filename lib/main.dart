991481840dad98c6a0b4296adefbfc7ab7dcc5a9
Kelvin <kelvindelima710@gmail.com>
2025-02-03 17:27:53 -0300

    fix: wrap MainApp with BlocProvider in runApp() method to guarantee that all widgets will have provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lapisco_challenge/app/data/blocs/city_search/city_search_bloc.dart';
 import 'package:lapisco_challenge/app/ui/pages/weather_info.dart';
 
 void main() {
-  runApp(const MainApp());
+  
+  runApp(BlocProvider<CitySearchBloc>( // Provider at the root of your app
+      create: (context) => CitySearchBloc(),
+      child: const MainApp(),
+    ),);
 }
 
 class MainApp extends StatelessWidget {
