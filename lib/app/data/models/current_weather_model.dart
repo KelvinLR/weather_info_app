edfa614199326e0d8eef3446bf0ada3f3e9d8130
Kelvin <kelvindelima710@gmail.com>
2025-02-02 21:39:03 -0300

    feat: add weatherCode field to CurrentWeatherModel

--- lib/app/data/models/current_weather_model.dart
+++ lib/app/data/models/current_weather_model.dart
@@ -2,6 +2,7 @@ class CurrentWeatherModel {
   final double currentTemperature;
   final int relativeHumidity;
   final double apparentTemperature;
+  final int weatherCode;
   final int isDay;
   final double precipitation;
   final double windSpeed;
@@ -10,6 +11,7 @@ class CurrentWeatherModel {
     required this.currentTemperature,
     required this.relativeHumidity,
     required this.apparentTemperature,
+    required this.weatherCode,
     required this.isDay,
     required this.precipitation,
     required this.windSpeed,
@@ -20,6 +22,7 @@ class CurrentWeatherModel {
         currentTemperature: map['temperature_2m'],
         relativeHumidity: map['relative_humidity_2m'],
         apparentTemperature: map['apparent_temperature'],
+        weatherCode: map['weather_code'],
         isDay: map['is_day'],
         precipitation: map['precipitation'],
         windSpeed: map['wind_speed_10m']);
