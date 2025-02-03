c4422db7168b25cd09c25b8b545a0338119f7a82
Kelvin <kelvindelima710@gmail.com>
2025-02-03 08:20:33 -0300

    feat: implement WeatherInfoPage

--- lib/app/ui/pages/weather_info.dart
+++ lib/app/ui/pages/weather_info.dart
@@ -1,39 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lapisco_challenge/app/data/blocs/city_search/city_search_state.dart';
+import 'package:lapisco_challenge/app/data/blocs/city_search/city_search_bloc.dart';
+import 'package:lapisco_challenge/app/data/blocs/city_search/city_search_event.dart';
+import 'package:lapisco_challenge/app/ui/widgets/forecast_graph/custom_bar_chart.dart';
+import 'package:lapisco_challenge/app/ui/widgets/weekly_summary_card/weekly_summary_card.dart';
 
-class WeatherInfoPage extends StatefulWidget {
+class WeatherInfoPage extends StatelessWidget {
   const WeatherInfoPage({super.key});
 
   @override
-  State<WeatherInfoPage> createState() => _WeatherInfoPageState();
-}
-
-class _WeatherInfoPageState extends State<WeatherInfoPage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Text('Weather Info App'),
-      ),
-      body: Column(
-        children: [
-          const TextField(
-            decoration: InputDecoration(
-                hintText: "Enter a location to show weather information"),
+    TextEditingController controller = TextEditingController();
+    double deviceWidth = MediaQuery.sizeOf(context).width;
+    double deviceHeight = MediaQuery.sizeOf(context).height;
+
+    return BlocProvider(
+      create: (context) => CitySearchBloc(),
+      child: Scaffold(
+        body: DecoratedBox(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF08244F),
+                Color(0xFF134CB5),
+                Color(0xFF0B42AB),
+              ],
+              stops: [0, 0.47, 1.00],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 1.0),
+            ),
           ),
-          Container(
-            child: Text("Melbourne"),
-          )
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          children: [
-            IconButton(
-              icon: Icon(Icons.abc_outlined),
-              onPressed: () {},
-            )
-          ],
+          child: Column(
+            children: [
+              Builder(builder: (context) {
+                return Padding(
+                  padding: EdgeInsets.only(
+                    top: deviceHeight * (44 / 858),
+                    left: deviceWidth * (41 / 423),
+                    right: deviceWidth * (41 / 423),
+                  ),
+                  child: TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      hintText: "Enter a location",
+                      filled: true,
+                      fillColor: const Color(0xFFFFFFFF),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                    ),
+                    onSubmitted: (value) {
+                      context
+                          .read<CitySearchBloc>()
+                          .add(SearchCityDataEvent(localName: value));
+                      controller.clear();
+                    },
+                  ),
+                );
+              }),
+              BlocBuilder<CitySearchBloc, CitySearchState>(
+                builder: (context, state) {
+                  if (state is CityDataLoading) {
+                    return const CircularProgressIndicator();
+                  } else if (state is CityDataLoaded) {
+                    return Column(
+                      children: [
+                        /*Text(state.cityData.name),
+                        Text("${state.cityData.latitude}"),
+                        Text("${state.cityData.longitude}"),
+                        Text("${state.cityData.elevation}"),
+                        Text("${state.currentWeatherData.apparentTemperature}"),
+                        Text("${state.currentWeatherData.currentTemperature}"),
+                        Text("${state.currentWeatherData.windSpeed}"),
+                        Text("${state.currentWeatherData.precipitation}"),
+                        Text("${state.currentWeatherData.isDay}"),
+                        Text("${state.currentWeatherData.relativeHumidity}"),
+                        Text("${state.dailyWeatherData.days}"),
+                        Text("${state.dailyWeatherData.maxTemperature}"),
+                        Text("${state.dailyWeatherData.minTemperature}"),
+                        Text("${state.dailyWeatherData.maxUvIndex}"),
+                        Text("${state.dailyWeatherData.windSpeed}"),*/
+                        Text(
+                          state.cityData.name,
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                            "currentTemperature: ${state.currentWeatherData.currentTemperature} °C"),
+                        Text(
+                            "apparentTemperature: ${state.currentWeatherData.apparentTemperature} °C"),
+                        Text(
+                            "windSpeed: ${state.currentWeatherData.windSpeed} km/h"),
+                        Text(
+                            "precipitation: ${state.currentWeatherData.precipitation} mm"),
+                        Text(
+                            "weatherCode: ${state.currentWeatherData.weatherCode}"),
+                        const Text('Last 7 days average temperature'),
+                        WeeklySummaryCard(
+                          customBarChart: CustomBarChart(
+                            weeklySummary:
+                                state.dailyWeatherData.maxTemperature,
+                            minY: state.dailyWeatherData.maxTemperature.reduce(
+                                (curr, next) => curr < next ? curr : next),
+                            maxY: state.dailyWeatherData.maxTemperature.reduce(
+                                (curr, next) => curr > next ? curr : next),
+                          ),
+                        )
+                      ],
+                    );
+                  } else if (state is CityDataError) {
+                    return const Text("Search failed");
+                  }
+                  return Container();
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
