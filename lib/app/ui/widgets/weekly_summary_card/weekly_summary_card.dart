675c4ff1876a9619defdfd6b1f9dd6bf12b0c3dc
Kelvin <kelvindelima710@gmail.com>
2025-02-03 15:26:23 -0300

    fix: change previous widget to CarouselSlider

--- lib/app/ui/widgets/weekly_summary_card/weekly_summary_card.dart
+++ lib/app/ui/widgets/weekly_summary_card/weekly_summary_card.dart
@@ -1,11 +1,12 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:lapisco_challenge/app/ui/widgets/forecast_graph/custom_bar_chart.dart';
 
 class WeeklySummaryCard extends StatelessWidget {
-  final CustomBarChart customBarChart;
+  final List<CustomBarChart> customBarChartList;
   const WeeklySummaryCard({
     super.key,
-    required this.customBarChart,
+    required this.customBarChartList,
   });
 
   @override
@@ -13,17 +14,28 @@ class WeeklySummaryCard extends StatelessWidget {
     double deviceWidth = MediaQuery.sizeOf(context).width;
     double deviceHeight = MediaQuery.sizeOf(context).height;
 
-    return Container(
-      width: deviceWidth * (343 / 423),
-      height: deviceHeight * (217 / 858),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(97, 255, 255, 255),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(12),
-        child: customBarChart,
-      ),
-    );
+    return CarouselSlider(
+        items: customBarChartList.map((item) {
+          return Builder(builder: (context) {
+            return Container(
+              width: deviceWidth * (343 / 423),
+              height: deviceHeight * (217 / 858),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(45, 255, 255, 255),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: item,
+              ),
+            );
+          });
+        }).toList(),
+        options: CarouselOptions(
+          viewportFraction: 1,
+          height: deviceHeight * (217 / 858),
+          enableInfiniteScroll: false,
+        ));
+    
   }
 }
